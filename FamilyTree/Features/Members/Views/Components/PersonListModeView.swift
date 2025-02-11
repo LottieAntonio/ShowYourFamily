7e84db330e01eb775dae8ba0b1cdfe8faa5b4ae8
林灿 <wz@192.168.5.42>
2025-02-11 18:12:50 +0800

    fix: 修复 PersonRow onTap 参数缺失问题

--- FamilyTree/Features/Members/Views/Components/PersonListModeView.swift
+++ FamilyTree/Features/Members/Views/Components/PersonListModeView.swift
@@ -6,7 +6,11 @@ struct PersonListModeView: View {
     var body: some View {
         List(viewModel.persons) { person in
             NavigationLink(value: person) {
-                PersonRow(person: person, isSelected: person.id == viewModel.selectedPerson?.id)
+                PersonRow(
+                    person: person,
+                    isSelected: person.id == viewModel.selectedPerson?.id,
+                    onTap: { viewModel.selectPerson(person) }
+                )
             }
         }
     }
